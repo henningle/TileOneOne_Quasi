99dc76e887ca4feba61effbe0bb34c2c8981157b
Henning Voss <henningle@users.noreply.github.com>
2025-02-07 15:48:08 -0500

    Add files via upload

---
+++ Fig_point_alpha_lattice.m
@@ -0,0 +1,241 @@
+% Fig_point_alpha_lattice.m, 7.2.2025
+% To show how points converge for point alpha
+
+% Please cite as "Henning U. Voss and Douglas J. Ballon, Quasilattices of the Spectre monotile, arXiv (2025)"
+% The license attached in GitHub applies, at https://github.com/henningle/TileOneOne_Quasi
+
+clear
+close all
+
+%% Parameters
+
+write_figures=true;
+
+Nmax=1;
+
+figsize=500;
+
+%% Tiling
+
+[S,centers,xangles,vecs,N,Ncorners]=TileOneOne_fc(Nmax);
+
+%% Paper figure
+
+markersize_dataunits=1; % The x dimension of unit tile is about 4.4
+markercolor=[222,105,54]/255;
+C=cos(30*pi/180);
+
+xys=[
+    % -2.78, -1.48 % alpha from oservation, minimum of pairwise entropy
+    % -2.7773, -1.4739 % alpha numetrically estimated below
+    - (27*3^(1/2))/28 - 31/28, 3^(1/2)/28 - 43/28 % alpha symbolically estimated below
+    ];
+
+x=xys(1,1); y=xys(1,2);
+
+h=figure('position',[100.,100.,figsize,figsize]);
+ax=axes('Position', [0.1, 0.1, .8, .8]);
+
+r=sqrt(x^2+y^2); phi=atan2(y,x);
+points=[centers(:,1)+r*cos(phi+xangles),centers(:,2)+r*sin(phi+xangles)];
+
+transparency=0;
+plot(ax,S(:,1),S(:,2),'k',LineWidth=.2,Color=transparency*[1,1,1]);
+hold on
+
+tmpplot=plot(ax,points(:,1), points(:,2),'.','Marker','o','MarkerFaceColor',markercolor,'MarkerEdgeColor',markercolor,'MarkerSize',1);
+
+% Correct scaling of markers. This is needed for defining it in terms of Spectre units
+x_range=diff(xlim(gca)); y_range=diff(ylim(gca));
+fig_position=get(gcf, 'Position'); % in pixels [left bottom width height]
+ax_position=get(gca, 'Position');  % normalized [left bottom width height]
+fig_width_inch=fig_position(3) * ax_position(3) / get(gcf, 'ScreenPixelsPerInch');
+fig_height_inch=fig_position(4) * ax_position(4) / get(gcf, 'ScreenPixelsPerInch');
+x_marker_size_points=markersize_dataunits / x_range * fig_width_inch * 72;
+y_marker_size_points=markersize_dataunits / y_range * fig_height_inch * 72;
+markersize_points=mean([x_marker_size_points, y_marker_size_points]);
+set(tmpplot, 'MarkerSize', markersize_points);
+axis off
+axis image
+
+% Plot connection arrow to center
+for kk=1:N
+    % if points(kk,1)>xstart && points(kk,1)<xstart+xydelta && points(kk,2)>ystart && points(kk,2)<ystart+xydelta
+    plot(ax,[centers(kk,1),points(kk,1)],[centers(kk,2),points(kk,2)],'k')
+    plot(ax,centers(kk,1),centers(kk,2),'Marker','o','MarkerFaceColor','k','MarkerEdgeColor','k','MarkerSize',6)
+    % text(ax,centers(kk,1)+.1,centers(kk,2),num2str(kk))
+end
+hold off
+
+set(h,'Color', [1 1 1])
+
+savefile=['png/fig_point_alpha'];
+if write_figures
+    print(h, '-dpng',  '-r400', [savefile '.png'])
+    saveas(h, [savefile '.fig'])
+end
+
+%% Analysis
+% In-tile coordinate system like in paper
+
+% disp(['x diameter = ' num2str(2*C+1.5) ', y diameter = ' num2str(0)])
+% disp(['x midpoint = ' num2str(C+.75) ', y center = ' num2str(0)])
+% disp(['x center = ' num2str(mean(-[0, 0, C, C-.5, C, 2*C, 2*C, 1+2*C, 2*C+1.5, C+1.5,C+1.5, C+.5, C, 0])) ...
+%     ', y center = ' num2str(mean([0, 1, 1.5, 1.5+C, 1.5+2*C, 1+2*C, 2*C, 2*C, C, C-.5, C-1.5, C-1.5, -1.5, -1]))]) % Long form
+disp(['x center = ' num2str(-(15*C+5.5)/14) ', y center = ' num2str((13*C+0.5)/14)]) % Short form
+
+h=figure('position',[100.,100.,2.8*figsize,2.8*figsize]);
+ax=axes('Position', [0.05, 0.05, .9, .9]); % Small margin
+
+plot(ax,S(:,1),S(:,2),'k',LineWidth=1,Color=transparency*[1,1,1]);
+hold on
+tmpplot=plot(ax,points(:,1), points(:,2),'.','Marker','o','MarkerFaceColor',markercolor,'MarkerEdgeColor',markercolor,'MarkerSize',1);
+set(tmpplot, 'MarkerSize', markersize_points);
+
+% Plot connection arrow to center
+for kk=1:N
+    plot(ax,[centers(kk,1),points(kk,1)],[centers(kk,2),points(kk,2)],'k')
+    plot(ax,centers(kk,1),centers(kk,2),'Marker','o','MarkerFaceColor','k','MarkerEdgeColor','k','MarkerSize',6)
+end
+
+% Label vertices numerically
+for kk=1:size(S,1) % 144
+    if abs(S(kk,1))<1e-4; S(kk,1)=0; end
+    if abs(S(kk,2))<1e-4; S(kk,2)=0; end
+    text(S(kk,1)+.06,S(kk,2)-.06,['(' num2str(S(kk,1),3) ',' num2str(S(kk,2),3) ')'],'Color','b','FontSize',12)
+end
+
+% S1: Label vertices algebraically
+xtexts={'0', '0', '-C', '-C+.5', '-C', '-2C', '-2C', '-2C-1', '-2C-1.5', '-C-1.5','-C-1.5', '-C-.5', '-C', '0'};
+ytexts={'0', '1', '1.5', 'C+1.5', '2C+1.5', '2C+1', '2C', '2C', 'C', 'C-.5', 'C-1.5', 'C-1.5', '-1.5', '-1'};
+for kk=1:14
+    text(S(kk,1)+.06,S(kk,2)+.1,['(' xtexts{kk} ',' ytexts{kk} ')'],'FontSize',12)
+    % text(S(kk,1)+.06,S(kk,2)+.08,[ xtexts{kk} ],'FontSize',12); text(S(kk,1)+.06,S(kk,2)-.08,[ ytexts{kk} ],'FontSize',12)
+end
+
+% S3: Label vertices algebraically
+xtexts={'-3C-3','-3C-3.5','-3C-3','-4C-3','-4C-3','-4C-2','-4C-1.5','-3C-1.5','-2C-1.5','-2C-2','-2C-1.5','-3C-1.5','-3C-1.5','-3C-2.5'};
+ytexts={'1.5','-C+1.5','-2C+1.5','-2C+1','-2C','-2C', '-C','-C-.5','-C','0','C','C+.5','C+1.5','C+1.5'};
+for kk=1:14
+    kk2=2*16+kk;
+    text(S(kk2,1)+.06,S(kk2,2)+.1,['(' xtexts{kk} ',' ytexts{kk} ')'],'FontSize',12)
+    % text(S(kk2,1)+.06,S(kk2,2)+.08,[ xtexts{kk} ],'FontSize',12); text(S(kk2,1)+.06,S(kk2,2)-.08,[ ytexts{kk} ],'FontSize',12)
+end
+
+% Test: Overlabel vertices numerically
+%{
+% First, put multipliers in
+xtexts={'-3*C-3','-3*C-3.5','-3*C-3','-4*C-3','-4*C-3','-4*C-2','-4*C-1.5','-3*C-1.5','-2*C-1.5','-2*C-2','-2*C-1.5','-3*C-1.5','-3*C-1.5','-3*C-2.5'};
+ytexts={'1.5','-C+1.5','-2*C+1.5','-2*C+1','-2*C','-2*C', '-C','-C-.5','-C','0','C','C+.5','C+1.5','C+1.5'};
+for kk=1:14
+       text(eval(xtexts{kk})+.06,eval(ytexts{kk})-.06,['(' num2str(eval(xtexts{kk}),3) ',' num2str(eval(ytexts{kk}),3) ')'],'Color','r','FontSize',12)
+end
+%}
+
+% S4: Label vertices algebraically
+xtexts={'-C-1.5','-C-2.5','-C-3','-2C-3','-3C-3','-3C-2.5','-3C-1.5','-3C-1.5','-2C-1.5','-2C-1','-2C','-2C','-C','-C-.5'};
+ytexts={'3C+1.5','3C+1.5','2C+1.5','2C+2','2C+1.5','C+1.5', 'C+1.5','C+.5','C','2C','2C','2C+1','2C+1.5','3C+1.5'};
+for kk=1:14
+    kk2=3*16+kk;
+    text(S(kk2,1)+.06,S(kk2,2)+.1,['(' xtexts{kk} ',' ytexts{kk} ')'],'FontSize',12)
+    % text(S(kk2,1)+.06,S(kk2,2)+.08,[ xtexts{kk} ],'FontSize',12); text(S(kk2,1)+.06,S(kk2,2)-.08,[ ytexts{kk} ],'FontSize',12)
+end
+
+% Test: Overlabel vertices numerically
+%{
+% First, put multipliers in
+xtexts={'-C-1.5','-C-2.5','-C-3','-2*C-3','-3*C-3','-3*C-2.5','-3*C-1.5','-3*C-1.5','-2*C-1.5','-2*C-1','-2*C','-2*C','-C','-C-.5'};
+ytexts={'3*C+1.5','3*C+1.5','2*C+1.5','2*C+2','2*C+1.5','C+1.5', 'C+1.5','C+.5','C','2*C','2*C','2*C+1','2*C+1.5','3*C+1.5'};
+for kk=1:14
+       text(eval(xtexts{kk})+.06,eval(ytexts{kk})-.06,['(' num2str(eval(xtexts{kk}),3) ',' num2str(eval(ytexts{kk}),3) ')'],'Color','r','FontSize',12)
+end
+%}
+
+% Label centers numerically
+% for kk=1:N
+%     text(centers(kk,1)+.06,centers(kk,2),['(' num2str(centers(kk,1),3) ',' num2str(centers(kk,2),3) ')'],'Color','b')
+%     text(ax,centers(kk,1)-.2,centers(kk,2),num2str(kk),'Color','r') % Tile number
+% end
+
+%% Label specific centers algebraically
+
+% Note: angles=[150,90,90,30,-30,-30,-90] for N=1 means that tile 3 is rotated 150 and tile 4 90 degrees, because tile 2 is part of M
+% [cos, -sin] [x]
+% [sin,  cos] [y]
+%{
+% Tile 3: 
+[cos(deg2rad(150)) -sin(deg2rad(150)) ] % = [-C -.5]
+[sin(deg2rad(150)) cos(deg2rad(150))]   %   [.5 -C]
+% Tile 4:
+[cos(deg2rad(90)) -sin(deg2rad(90)) ] % = [0 -1]
+[sin(deg2rad(90)) cos(deg2rad(90))]   %   [1  0]
+%}
+
+kk=1;
+T10=[0;0]; % Origin of tile 1
+C1=[-(15*C+5.5)/14; (13*C+0.5)/14]; % Center of tile 1
+text(centers(kk,1)+.06,centers(kk,2)+.08, '-(15*C+5.5)/14','Color','r'); text(centers(kk,1)+.06,centers(kk,2)-0.08, '(13*C+0.5)/14','Color','r')
+
+axis image
+hold off
+
+% kk=3;
+% How to get center for tile 3
+% The centers for tiles > 1 are not computed by the mean values of the vertices but but by the shift and rotation of tile 1
+% Use the relationship to the origin, rotate this vector by 90 degrees, and shift it by the new origin of tile 3
+M3=[-C -.5; .5 -C]; % Rotation of tile 3
+T30=[-3*C-3;1.5]; % Origin of tile 3
+C3=M3*C1+T30; % Center of tile 3
+
+% kk=4;
+M4=[0 -1; 1 0];
+T40=[-C-1.5;3*C+1.5];
+C4=M4*C1+T40;
+
+set(h,'Color', [1 1 1]) % make white background
+
+savefile=['png/fig_point_alpha_shifts'];
+if write_figures
+    print(h, '-dpng',  '-r400', [savefile '.png'])
+    saveas(h, [savefile '.fig'])
+end
+
+%% Determine point alpha as the common point for decoration of tiles 3 and 4
+
+% Tile 1: alpha1=alpha relative to T01 = C1+[x;y]. Nothe this is in absolute coordinates in tile 1; should be [-4.1;-0.64] in absolute coords, where the decoration is
+% Tile 2 is not needed (this is the lower body of the Mystic)
+% Tile 3: alpha3=M3*(alpha relative to C1)+T30
+% Tile 4: alpha4=M4*(alpha relative to C1)+T40
+
+% First, test if present alpha fulfills this. The common point is [-1.72;0.0039]
+% alpha3=M3*([x;y]+C1)+T30
+% alpha4=M4*([x;y]+C1)+T40
+% Yes, they are very similar but not exact
+
+% Find exact value by setting alpha3=alpha4 and solving for x,y
+
+zeta=inv(M4-M3)*(T30-T40);
+% zeta=inv([C, -0.5; 0.5, C])*[-2*C-1.5;-3*C] % same
+alpha_num=zeta-C1 %   -2.7773, -1.4739
+
+% But what is the algebraic value?
+% Do the exact same thing in symbolic algebra
+
+Csym=sym(cos(pi/6)); % sqrt(3)/2 = cos30 = 0.8660
+
+C1sym=[-(15*Csym+11/2)/14; (13*Csym+1/2)/14]; % Center of tile 1
+M3sym=[-Csym -1/2; 1/2 -Csym]; % Rotation of tile 3
+T30sym=[-3*Csym-3; 3/2]; % Origin of tile 3
+
+M4sym=[0 -1; 1 0];
+T40sym=[-Csym-3/2;3*Csym+3/2];
+
+% Solving the inverse equation; resolve to alpha
+zetasym=inv(M4sym-M3sym)*(T30sym-T40sym);
+alpha_sym=zetasym-C1sym ;
+alpha_sym=simplify(alpha_sym);
+pretty(alpha_sym)
+alpha_sym_eval=eval(alpha_sym)
+
+% All the other merged points result from the same geometry modulo shift and rotation, as can be seen from the supplementary figure
+% QED
---
+++ TileOneOne_fc.m
@@ -0,0 +1,241 @@
+% TileOneOne_fc.m, 5.12.2024
+
+% Please cite as "Henning U. Voss and Douglas J. Ballon, Quasilattices of the Spectre monotile, arXiv (2025)"
+% The license attached in GitHub applies, at https://github.com/henningle/TileOneOne_Quasi
+
+function [S,centers,xangles,vecs,N,Ncorners]=TileOneOne_fc(Nmax)
+
+plotting=false;
+
+%% Parameters
+
+plotkeypoints=false; % Set to false if plotting key points not wanted
+
+%% Main script
+
+% Tile placement function
+% Each tile has 3 components: x, y, key point number (0 for no key point, 1 to 4 if key point)
+% The placetile function takes in a matrix of size (number of rows, 3)
+% The first 2 columns are the coordinates of the tiling, the third is a label of key points
+placetile=@(x, angle, coord) ...
+    [transpose( [cos(angle*pi/180), -sin(angle*pi/180); ...
+    sin(angle*pi/180), cos(angle*pi/180)]*x(:,1:2)' ...
+    +transpose(repmat(coord,[size(x,1),1]))) , x(:,3)];
+
+mirror=@(x) [transpose([-1, 0; 0, 1]*(x(:,1:2)')),  x(:,3)];
+
+Ncorners=15; % 14 vertices + 1 vertex to get back to origin
+
+% Number of expected tiles for each iteration
+nS=zeros(1,Nmax+1);
+nM=nS;
+nS(1)=1; nM(1)=2; % This is index 0
+for n=2:Nmax+1
+    nS(n)=nM(n-1)+7*nS(n-1);
+    nM(n)=nM(n-1)+6*nS(n-1);
+end
+nS=nS(2:end);
+% nM=nM(2:end);
+disp(['Predicted number of tiles for N = 1, 2, ...: ' num2str(nS)])
+
+%% S0 and M0
+
+% Some definitions
+cos30=cos(30*pi/180); sin30=sin(30*pi/180);
+cos60=cos(60*pi/180); sin60=sin(60*pi/180);
+cos120=cos(120*pi/180); sin120=sin(120*pi/180);
+cos150=cos(150*pi/180); sin150=sin(150*pi/180);
+cos210=cos(210*pi/180); sin210=sin(210*pi/180);
+cos240=cos(240*pi/180); sin240=sin(240*pi/180);
+cos300=cos(300*pi/180); sin300=sin(300*pi/180);
+cos330=cos(330*pi/180); sin330=sin(330*pi/180);
+
+% One Tile(1,1)
+% Each tile has 14 edges and 15 points, the last point for closure
+% Plus one NaN for separation of tiles while plotting
+% x-components of specter S
+Stmp1=[ 0
+    0
+    cos30
+    cos30+cos120
+    cos30+cos120+cos60
+    cos30+cos120+cos60+cos330
+    cos30+cos120+cos60+cos330
+    cos30+cos120+cos60+cos330+1
+    cos30+cos120+cos60+cos330+1+cos300
+    cos30+cos120+cos60+cos330+1+cos300+cos210
+    cos30+cos120+cos60+cos330+1+cos300+cos210
+    cos30+cos120+cos60+cos330+cos300+cos210
+    cos30+cos120+cos60+cos330+cos300+cos210+cos240
+    cos30+cos120+cos60+cos330+cos300+cos210+cos240+cos150
+    cos30+cos120+cos60+cos330+cos300+cos210+cos240+cos150
+    NaN];
+% y-components of specter S
+Stmp2=[0
+    1
+    1+sin30
+    1+sin30+sin120
+    1+sin30+sin120+sin60
+    1+sin30+sin120+sin60+sin330
+    sin30+sin120+sin60+sin330
+    sin30+sin120+sin60+sin330
+    sin30+sin120+sin60+sin330+sin300
+    sin30+sin120+sin60+sin330+sin300+sin210
+    sin30+sin120+sin60+sin330+sin300+sin210-1
+    sin30+sin120+sin60+sin330+sin300+sin210-1
+    sin30+sin120+sin60+sin330+sin300+sin210-1+sin240
+    sin30+sin120+sin60+sin330+sin300+sin210-1+sin240+sin150
+    sin30+sin120+sin60+sin330+sin300+sin210+sin240+sin150
+    NaN];
+% Specter S
+S=[Stmp1,Stmp2];
+
+% Augment S with key points to be carried over during iterative construction
+keypoints=zeros(16,1); keypoints(4)=1; keypoints(6)=2; keypoints(8)=3; keypoints(14)=4;
+S=[S,keypoints];
+keyind=find(S(:,3)>0)'; % The indexes that point to key points
+% S(keyind,3)' % Should be 1,2,3,4 for iteration 0
+
+% The Mystic
+M=[S; placetile(S,-30,[sqrt(3)/2,-1.5])];
+M(:,3)=0; % M should not have key points not to contaminate S
+
+%% Iteration loop
+
+for N=1:Nmax
+
+    S=mirror(S);
+    M=mirror(M);
+
+    % The first two iterations require explicit key points for the next iteration
+    switch N
+        case 1
+            Sind=[4,6,8,14]; % Key points on S0, for making S1
+            Mind=Sind+16;
+            angles=[150,90,90,30,-30,-30,-90];
+        case 2
+            Sind=[84,118,132,38]; % Key points on S1, for making S2
+            % Indexing changes in M2 due to the missing patch
+            Mind=[84-16,118-16,132-16,38]; %
+            angles=[120,60,60,0,-60,-60,-120]; % Conjecture: Angles converged from here on
+    end
+
+    % For iteration 3 and higher, keypoints are computed from the previous two iterations
+    if N > 2
+        Sind=circshift(keyind,-1);
+        Mind(1:3)=Sind(1:3)-16*nS(N-2);
+    end
+
+    % Define some tiling cells
+    cell0=M;
+    cell1=placetile([S(:,1:2)-S(Sind(3),1:2), S(:,3)],angles(1), M(Mind(3),1:2));
+    cell2=placetile([S(:,1:2)-S(Sind(2),1:2), S(:,3)],angles(2),cell1(Sind(4),1:2));
+    cell3=placetile([S(:,1:2)-S(Sind(3),1:2), S(:,3)],angles(3),cell2(Sind(1),1:2));
+    cell4=placetile([S(:,1:2)-S(Sind(2),1:2), S(:,3)],angles(4),cell3(Sind(4),1:2));
+    cell5=placetile([S(:,1:2)-S(Sind(2),1:2), S(:,3)],angles(5),cell4(Sind(4),1:2));
+    cell6=placetile([S(:,1:2)-S(Sind(3),1:2), S(:,3)],angles(6),cell5(Sind(1),1:2));
+    cell7=placetile([S(:,1:2)-S(Sind(2),1:2), S(:,3)],angles(7),cell6(Sind(4),1:2));
+
+    %% Assemble S and M
+
+    S=[cell0 % 2x16 elements because this is the mystic M
+        cell1 % 16 elements
+        cell2
+        cell3
+        cell4
+        cell5
+        cell6
+        cell7
+        ];
+    M=[cell0 % 2x16 elements
+        cell1 % 16 elements
+        cell2
+        cell4 % Note we skipped cell3
+        cell5
+        cell6
+        cell7
+        ];
+    M(:,3)=0; % Again, M should not have key points
+
+    if plotting
+        figure('position',[500.,100.,1000,1000]);
+        plot(S(:,1),S(:,2),'k',LineWidth=.1);
+        axis image
+        title(['S' num2str(N)])
+    end
+
+    %% Key points of this iteration
+
+    keyind=find(S(:,3)>0); % Always 7*4=28 elements after pruning previous iteration
+
+    if plotting
+        if plotkeypoints
+            % Plot all keypoints before pruning, in red
+            hold on
+            for n=1:4:length(keyind)
+                plot(S([keyind(n:n+3);keyind(n)],1),S([keyind(n:n+3);keyind(n)],2),'ro-');
+            end
+        end
+    end
+
+    %% Key points for next iteration
+
+    % At the beginning, there are always 9*4=36 key points. At the end, there should be 4
+    keyind2=keyind([2,13,22,25]); % Read off from Smith23b Fig. A1.2a and previous plot
+    if N>1; keyind2=keyind([2,13,22,25]+1); end % Don't ask
+    % S(keyind2,3)' % Should be (2,1,2,1) for iteration 1 and (3,2,3,2) for iteration >= 2
+
+    S(:,3)=0; % Clean out this iteration's key points
+
+    % Set new keypoints for constructing the next iteration
+    S(keyind2(1),3)=1;
+    S(keyind2(2),3)=2;
+    S(keyind2(3),3)=3;
+    S(keyind2(4),3)=4;
+
+    keyind=find(S(:,3)>0); % Must be 38 84 118 132 for iteration 2
+
+    if plotting
+        if plotkeypoints
+            % Plot only key points taken over to next iteration, in blue
+            plot(S([keyind;keyind(1)],1),S([keyind;keyind(1)],2),'bo-');
+        end
+    end
+
+    % Count tiles
+    nStrue=size(S,1)/16;
+    disp(['Iteration ' num2str(N) ' has ' num2str(nStrue) ' tiles'])
+
+end
+
+% Make the turtle always look to the left. It has to be inverted for even Nmax
+if not(mod(Nmax,2))
+    disp('Flipping pattern for even Nmax')
+    S=mirror(S);
+end
+
+S=S(:,1:2); % Saving only the lattice proper
+N=nStrue; % Size or RF array
+
+%% Further outputs
+
+centers=zeros(N,2);
+for n=1:N % Loop through resonators
+    centers(n,:)=[mean(S((n-1)*16+1:(n-1)*16+14,1)),mean(S((n-1)*16+1:(n-1)*16+14,2))]; % When computing the center, the last point needs to be removed becasue it is repeated
+end
+
+% Angles
+% Paralell to x axis to make the angles multiples of 30 degrees
+% 2 vectors pointing to the base edge define the base vector, which serves as a reference for the angle
+% Astonishingly, there is no need to correct for flipped S for odd Nmax
+vecs=NaN*zeros(3*N,2); % Has NaNs for easier plotting
+xangles=zeros(N,1);
+for ii=1:N
+    vecs(3*ii-2,:)=[S((ii-1)*(Ncorners+1)+11,1),S((ii-1)*(Ncorners+1)+11,2)]; % vector 1
+    vecs(3*ii-1,:)=[S((ii-1)*(Ncorners+1)+12,1),S((ii-1)*(Ncorners+1)+12,2)]; % vector 2
+    % Angles
+    v1=vecs(3*ii-2,:); v2=vecs(3*ii-1,:);
+    vx=v2(1)-v1(1); vy=v2(2)-v1(2); % First get direction vector from v1 to v2
+    xangles(ii)=atan2(vy, vx);
+end
+% rad2deg(xangles(1:10))' % Must start with 0
